22de407dcb3193e701588f489c2030dcdc179ac6
zhangting <1092923594@qq.com>
2024-02-27 20:57:37 +0800

    update

--- database/app.js
+++ database/app.js
@@ -1,6 +1,7 @@
 const http = require('http');
 const mongoose = require('mongoose');
 const url = require('url');
+const querystring = require('querystring');
 
 // 数据库连接
 mongoose.connect('mongodb://localhost/playground')
@@ -38,16 +39,17 @@ const User = mongoose.model('User', UserSchema);
 const app = http.createServer();
 
 // 添加事件
-app.on('request', (req, res) => {
+app.on('request', async (req, res) => {
   // 请求方式
   const method = req.method;
   
   let { pathname } = url.parse(req.url);
   // pathname = pathname === '/' ? 'default.html' : pathname;
-
-  if(method == 'GET') {
+  console.info(pathname, method)
+  if(method === 'GET') {
     // 呈现用户列表页面
-    if(pathname == 'list') {
+    if(pathname === '/list') {
+      let users = await User.find();
       let list = `
       <!DOCTYPE html>
       <html lang="en">
@@ -70,25 +72,52 @@ app.on('request', (req, res) => {
               <td>邮箱</td>
               <td>操作</td>
             </tr>
-            <tr>
-              <td>张三</td>
-              <td>22</td>
-              <td><span>测试</span></td>
-              <td>zhangting@mi.com</td>
+      `
+      users.forEach(item => {
+        let hobbySpan = ``
+        item.hobbies.forEach(hobby => {
+          hobbySpan += `<span>${hobby}</span>`
+        })
+        list += `
+          <tr>
+              <td>${item.name}</td>
+              <td>${item.age}</td>
+              <td>${hobbySpan}</td>
+              <td>${item.email}</td>
               <td>
                 <a href="" class="btn btn-danger">删除</a>
                 <a href="" class="btn btn-success">修改</a>
               </td>
             </tr>
+          `
+      })
+      list += `
           </table>
         </div>
       </body>
       </html>
       `;
       res.end(list);
+    } else if(pathname === '/add') {
+
     }
   } else if(method == 'POST') {
+    if(pathname === '/add') {
+      let formData = ''
+      res.on('data', (param) => {
+        formData += param
+      })
 
+      res.on('end', async () => {
+        console.log(querystring.parse(formData))
+        // 将用户提交的信息添加到数据库中
+        await User.create(user);
+        res.writeHead(301, {
+          Location: '/list'
+        });
+        res.end();
+      })
+    }
   }
 })
 
